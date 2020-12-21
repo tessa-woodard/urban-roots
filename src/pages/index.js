15d16e0d416078d07c2de3c053f3a4c18170d9cc
Tessa Woodard <tessawoodard@outlook.com>
2020-12-21 17:37:23 -0500

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,8 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import BackgroundSection from "../components/Globals/BackgroundSection"
 
+import Inspiration from "../components/Inspiration/Inspiration"
+
 const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
@@ -13,6 +15,7 @@ const IndexPage = ({ data }) => (
       title="Urban Roots"
       styleClass="default-background"
     />
+    <Inspiration />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
   </Layout>
