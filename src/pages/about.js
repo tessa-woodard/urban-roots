fa922bf13d388e19497946d6a081e0ce53ec5738
Tessa Woodard <tessawoodard@outlook.com>
2020-12-21 17:51:33 -0500

    wip

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,12 +3,12 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-import About from "../components/About/About"
+// import About from "../components/About/About"
 
 const AboutPage = () => (
   <Layout>
     <SEO title="About" />
-    <About />
+    {/* <About /> */}
   </Layout>
 )
 
