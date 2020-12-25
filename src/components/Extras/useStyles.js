235624e4cef18e7f18784e25f96500995084474a
Tessa Woodard <tessawoodard@outlook.com>
2020-12-25 18:09:02 -0500

    shipping page done

--- src/components/Extras/useStyles.js
+++ src/components/Extras/useStyles.js
@@ -316,4 +316,48 @@ export const useStyles = makeStyles(theme => ({
       height: 350,
     },
   },
+
+  secondAbout: {
+    display: "flex",
+    justifyContent: " space-around",
+    margin: "250px 0",
+    [theme.breakpoints.down("md")]: {
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+  },
+
+  secondTitleShipping: {
+    fontSize: "4em",
+    lineHeight: "1.4em",
+    marginBottom: 40,
+    fontFamily: "'Playfair Display', serif",
+    letterSpacing: "1.1px",
+    marginTop: 0,
+    [theme.breakpoints.down("md")]: {
+      fontSize: "1.7em",
+    },
+  },
+
+  secondText: {
+    fontSize: 17,
+    lineHeight: "29px",
+    marginBottom: 40,
+    color: "rgba(0,0,0,0.5)",
+    textAlign: "justify",
+  },
+
+  imgBlockShipping: {
+    width: "48%",
+    backgroundImage:
+      'url("https://github.com/tessa-woodard/urban-roots-revised/blob/main/src/assets/shipping.png?raw=true")',
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    [theme.breakpoints.down("md")]: {
+      width: "60%",
+      height: 350,
+    },
+  },
 }))
